1da81009f15f48f4b333361cb2a3aeb4eaee9b4b
root <zahid.chowdhury023@gmail.com>
2023-11-19 02:58:27 +0600

    temp commit

--- src/main/java/com/ghuddy/backendapp/tours/es/dto/data/ESTransferPackageData.java
+++ src/main/java/com/ghuddy/backendapp/tours/es/dto/data/ESTransferPackageData.java
@@ -48,6 +48,5 @@ public class ESTransferPackageData {
         this.tripType = transferPackageEntity.getTripType();
         this.unitPrice = transferPackageEntity.getUnitPrice();
         this.suitableForPersons = transferPackageEntity.getSuitableForPersons();
-        //
     }
 }
